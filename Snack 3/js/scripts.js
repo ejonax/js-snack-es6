b480b446d0de5fb2f31448305cd1e493e346ffc7
ejonax <ejonax@yahoo.it>
2025-01-10 19:49:50 +0100

    feat: filter method for snack3

--- Snack 3/js/scripts.js
+++ Snack 3/js/scripts.js
@@ -1,54 +1,31 @@
-const animali=
-{
-  animale1 :{ 
-    nome: "leone", 
-    famiglia: "felidi", 
-    classe: "mammiferi" 
-  },
-  animale2 :{ 
-    nome: 'cane', 
-    famiglia: 'canidi', 
-    classe: 'mammiferi' 
-},
-  animale3: { 
-    nome: 'gallina', 
-    famiglia: 'fasianidi', 
-    classe: 'uccelli'
-},
-animale4 :{ 
-    nome: "gatto", 
-    famiglia: "felidi", 
-    classe: "mammiferi" 
-  },
-  animale5 :{ 
-    nome: 'cigno', 
-    famiglia: 'fasianidi', 
-    classe: 'uccelli'
+const animals=
+[
+  { nome: "leone", famiglia: "felidi", classe: "mammiferi" },
+  { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+  { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli'},
+  { nome: "gatto", famiglia: "felidi", classe: "mammiferi" },
+  { nome: 'cigno', famiglia: 'fasianidi', classe: 'uccelli'},
+  { nome: 'balena', famiglia: 'Cetacea', classe: 'mammiferi'},
+
+]
+
+//FOREACH METHOD
+//ciclo forearch dove si controlla dentro if se la callse è mammiferi
+//creo un nuovo array onlyMammiferi, faccio push ogni elemento che passa il controlllo del if
+let onlyMammiferi=[];
+animals.forEach(element => {
+  if (element.classe==="mammiferi")
+  {
+    onlyMammiferi.push(element);
+  }
 }
-}
+);
 
+console.log(onlyMammiferi);
 
-const mammiferi=[]; // creamo la nuova array mammiferi dove aggiungeremo gli elementi dell' animali che sono mammiferi
-for (const x in animali) {
-        const element = animali[x]; // è elemento di turno che controlliamo per ogni iterazione
-        if (element.classe=="mammiferi"){ // se il value legato alla propertie classe è uguale a mammiferi allora 
-            console.log(element); //la stampiamo
-            mammiferi.push({element:element});//la aggiungiamo alla nuova array mammiferi che abbiamo creato
-        }
-}
-console.log(mammiferi);//stampiamo tutta l'array mammiferi
-
-/* Secondo metodo dove stampo il nome degli mammiferi sul index.html*/
-const mam=[]; // creamo la nuova array mammiferi dove aggiungeremo solo il nome degli animali che sono mammiferi
-for (const x in animali) {
-        const element = animali[x]; 
-        if (element.classe=="mammiferi"){ // se il value legato alla propertie classe è uguale a mammiferi allora 
-            let nomeAnimale=element.nome;//prendo solo il nome dell'elemento animali
-            console.log(nomeAnimale);
-            mam.push(nomeAnimale);
-        }
-    }
-//stampo la lista degli mammiferi su html
-document.getElementById("demo2").innerHTML = "<p> La lista degli animali mammiferi è: </p>" + mam; 
-
-  
+//FILTER METHOD
+//siccome animals è un array si uo usare il methodo filter
+//come filter si controlla se la classe è mammiferi
+//si stampa la nuova array 
+var mamAnimals=animals.filter(item=>item.classe==="mammiferi");
+console.log(mamAnimals);
