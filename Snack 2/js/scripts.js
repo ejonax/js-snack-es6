83631632aba69fcb58e03a1bbedda5305c01c1a1
ejonax <ejonax@yahoo.it>
2025-01-10 20:21:09 +0100

    feat: forEach cicle snack2

--- Snack 2/js/scripts.js
+++ Snack 2/js/scripts.js
@@ -1,27 +1,32 @@
 const stringhe= ['pippo', 'PLUTO', 'Paperino','paperinO','PaPeRinO'] ;
-const capitalizedStringhe=[];
+const capitalizedForEach=[];
+const capitalizedFor=[];
 
 function capitalize(stringa) {
-    return stringa.charAt(0).toUpperCase() + stringa.slice(1).toLowerCase();
-    // la var stringa lo trata come un'array
     // il primo char che lo trova su index 0 lo fa maiuscola
     //il resto dell'array lo fa minuscola
+    return stringa.charAt(0).toUpperCase() + stringa.slice(1).toLowerCase();
 }
-/*
-console.log(capitalize("pippo")) ; // => "Pippo"
-console.log(capitalize("PLUTO"));// => "Pluto
-console.log(capitalize("Paperino"));  // => "Paperino"
-console.log(capitalize("paperinO"));  // => "Paperino"
-console.log(capitalize("PaPeRinO"));  // => "Paperino"
-*/
 
+//FOREACH METHOD
+//chiamiamo la funzione capitalize per ogni elemento del array e si fa push su nuovo array
+stringhe.forEach(element => {
+    capitalizedForEach.push(capitalize(element));
+});
+
+console.log(capitalizedForEach);
+
+//FOR CICLE METHOD
+//chiamiamo la funzione capitalize per ogni elemento del array
 for (let index = 0; index <   stringhe.length; index++) {
     const element = stringhe[index];
     const capitalizedElement=capitalize(element);
-    console.log(capitalizedElement);
-    capitalizedStringhe.push(capitalizedElement);
+    capitalizedFor.push(capitalizedElement);
 }
-console.log(capitalizedStringhe);
 
+console.log(capitalizedFor);
+
+//stampiamo le nuove array anche sul lato HTML
 document.getElementById("demo1").innerHTML = "<p> Il primo array  delle stringhe è: </p> <u>" + stringhe + "</u>"; 
-document.getElementById("demo2").innerHTML = "<p> Il secondo array formattando le stringhe del primo array in minuscolo e con l'iniziale maiuscola è: </p> <u>" + capitalizedStringhe + "</u>"; 
+document.getElementById("demo2").innerHTML = "<p> Il nuovo array creato usando il ciclo FOREACH e la funzione capitalize è: </p> <u>" + capitalizedForEach + "</u>"; 
+document.getElementById("demo3").innerHTML = "<p> Il nuovo array creato usando il ciclo FOR e la funzione capitalize è:</p> <u>" + capitalizedFor + "</u>"; 
